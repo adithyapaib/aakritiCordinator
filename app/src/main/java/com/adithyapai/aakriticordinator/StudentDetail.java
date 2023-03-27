4b7c8941b32d9db7aa270c020d63f77049cf5d1c
Adithya Pai B <paiadithya26@gmail.com>
2023-03-27 23:34:49 +0530

    Updated Buttons and contacts.html

--- app/src/main/java/com/adithyapai/aakriticordinator/StudentDetail.java
+++ app/src/main/java/com/adithyapai/aakriticordinator/StudentDetail.java
@@ -51,129 +51,119 @@ public class StudentDetail extends AppCompatActivity {
             barLauncher.launch(options);
         });
 
-      barLauncher  = registerForActivityResult(new ScanContract(), result -> {
+        barLauncher = registerForActivityResult(new ScanContract(), result -> {
             if (result.getContents() != null) {
                 String url = result.getContents();
-                startActivity(new Intent(this, StudentDetail.class).putExtra("data","https://aakriti23.canaraengineering.in/user.php?aid="+ url));
+                startActivity(new Intent(this, StudentDetail.class).putExtra("data", "https://aakriti23.canaraengineering.in/user.php?aid=" + url));
             }
         });
 
 
-
         String url;
 
-         url =  getIntent().getStringExtra("data");
-         if (url == null || url.isEmpty()){
+        url = getIntent().getStringExtra("data");
+        if (url == null || url.isEmpty()) {
             Toast.makeText(this, "Invalid ID", Toast.LENGTH_SHORT).show();
             finish();
         }
 
         RequestQueue queue = Volley.newRequestQueue(this);
-        @SuppressLint({"DefaultLocale", "SetTextI18n"}) StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                response -> {
+        @SuppressLint({"DefaultLocale", "SetTextI18n"}) StringRequest stringRequest = new StringRequest(Request.Method.GET, url, response -> {
+            try {
+
+                JSONObject jsonObject;
+                jsonObject = new JSONObject(response);
+                String id, email, name, phone, inst, course, usn, branch, paid, pending;
+                ImageView imageView = findViewById(R.id.userimg);
+                imageView.setClipToOutline(true);
+                String imgsrc = "https://aakriti23.canaraengineering.in/user_images/" + jsonObject.getString("photo");
+                Picasso.get().load(imgsrc).into(imageView);
+                email = jsonObject.getString("email");
+                name = jsonObject.getString("Name");
+                phone = jsonObject.getString("phone");
+                inst = jsonObject.getString("inst");
+                course = jsonObject.getString("course");
+                usn = jsonObject.getString("usn");
+                branch = jsonObject.getString("branch");
+                paid = jsonObject.getString("paid");
+                pending = jsonObject.getString("pending");
+                id = String.format("%05d", Integer.parseInt(jsonObject.getString("id")));
+                // check if size of branch is greater than 20
+                if (branch.length() > 20) {
+                    branch = branch.substring(0, 20);
+                }
+                if (inst.length() > 20) {
+                    inst = inst.substring(0, 20);
+                }
+
+
+                if (Integer.parseInt(paid) >= 300) {
+                    MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.success);
+                    View view = findViewById(R.id.view);
+                    view.setBackgroundColor(Color.parseColor("#54B435"));
+                    Button valid = findViewById(R.id.valid);
+                    valid.setVisibility(View.VISIBLE);
+                    valid.setText("Payment Done");
+                    valid.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.valid, 0);
+                    MediaPlayer mp = MediaPlayer.create(this, R.raw.success);
+                    mp.start();
+                    imageView = findViewById(R.id.userimg);
+                    imageView.setClipToOutline(true);
+                    imgsrc = "https://aakriti23.canaraengineering.in/user_images/" + jsonObject.getString("photo");
+                    Picasso.get().load(imgsrc).into(imageView);
+                    email = jsonObject.getString("email");
+                    name = jsonObject.getString("Name");
+                    phone = jsonObject.getString("phone");
+                    inst = jsonObject.getString("inst");
+                    course = jsonObject.getString("course");
+                    usn = jsonObject.getString("usn");
+                    branch = jsonObject.getString("branch");
+                    paid = jsonObject.getString("paid");
+                    pending = jsonObject.getString("pending");
+                    id = String.format("%05d", Integer.parseInt(jsonObject.getString("id")));
+                    String formattedText = "  <h2> ID: AAK" + id + "</h2>\n" + "  Name:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + name + "  <br />\n" + "  Email:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + email + "  <br />\n" + "  Phone:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a href=tel:" + phone + ">" + phone + "</a>" + "  <br />\n" + "  Institute:&nbsp;&nbsp;" + inst.substring(0, 20) + "  <br />\n" + "  Course:&nbsp;&nbsp;" + course + "  <br />\n" + "  USN:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + usn.toUpperCase(Locale.ROOT) + "  <br />\n" + "  Branch:&nbsp;&nbsp;" + branch + "  <br />\n" + "  Paid:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + paid + "  <br />\n" + "  Pending:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + pending;
+                    ttv.setText(Html.fromHtml(formattedText));
+
+
+                } else if (Integer.parseInt(paid) < 300) {
+
                     try {
+                        String formattedText = "  <h2> ID: AAK" + id + "</h2>\n" + "  Name:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + name + "  <br />\n" + "  Email:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + email + "  <br />\n" + "  Phone:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a href=tel:" + phone + ">" + phone + "</a>" + "  <br />\n" + "  Institute:&nbsp;&nbsp;" + inst.substring(0, 20) + "  <br />\n" + "  Course:&nbsp;&nbsp;" + course + "  <br />\n" + "  USN:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + usn.toUpperCase(Locale.ROOT) + "  <br />\n" + "  Branch:&nbsp;&nbsp;" + branch + "  <br />\n" + "  Paid:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + paid + "  <br />\n" + "  Pending:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + pending;
+                        ttv.setText(Html.fromHtml(formattedText));
 
-                        JSONObject jsonObject;
-                        jsonObject = new JSONObject(response);
-                        String id, email, name, phone, inst, course, usn, branch, paid, pending;
-                        ImageView imageView = findViewById(R.id.userimg);
-                        imageView.setClipToOutline(true);
-                        String imgsrc =  "https://aakriti23.canaraengineering.in/user_images/"+jsonObject.getString("photo");
-                        Picasso.get().load(imgsrc).into(imageView);
-                        email = jsonObject.getString("email");
-                        name = jsonObject.getString("Name");
-                        phone = jsonObject.getString("phone");
-                        inst = jsonObject.getString("inst");
-                        course = jsonObject.getString("course");
-                        usn = jsonObject.getString("usn");
-                        branch = jsonObject.getString("branch");
-                        paid = jsonObject.getString("paid");
-                        pending = jsonObject.getString("pending");
-                        id = String.format("%05d", Integer.parseInt(jsonObject.getString("id")));
-                        // check if size of branch is greater than 20
-                        if (branch.length() > 20){
-                            branch = branch.substring(0,20);
-                        }
-                        if (inst.length() > 20){
-                            inst = inst.substring(0,20);
-                        }
-
-
-                        if (Integer.parseInt(paid) >=300){
-                            MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.success);
-                            View view = findViewById(R.id.view);
-                            view.setBackgroundColor(Color.parseColor("#54B435"));
-                            Button valid = findViewById(R.id.valid);
-                            valid.setVisibility(View.VISIBLE);
-                            valid.setText("Payment Done");
-                            valid.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.valid, 0);
-                            MediaPlayer mp = MediaPlayer.create(this, R.raw.success);
-                            mp.start();
-
-
-
-
-                        }else if (Integer.parseInt(paid) < 300){
-
-                          try {
-                              String formattedText =
-                                      "  <h2> ID: AAK" + id + "</h2>\n" +
-                                              "  Name:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + name +
-                                              "  <br />\n" +
-                                              "  Email:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + email +
-                                              "  <br />\n" +
-                                              "  Phone:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a href=tel:" + phone + ">" + phone + "</a>" +
-                                              "  <br />\n" +
-                                              "  Institute:&nbsp;&nbsp;" + inst.substring(0,20) +
-                                              "  <br />\n" +
-                                              "  Course:&nbsp;&nbsp;" + course +
-                                              "  <br />\n" +
-                                              "  USN:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + usn.toUpperCase(Locale.ROOT) +
-                                              "  <br />\n" +
-                                              "  Branch:&nbsp;&nbsp;" + branch+
-                                              "  <br />\n" +
-                                              "  Paid:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" +  paid +
-                                              "  <br />\n" +
-                                              "  Pending:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + pending;
-                              ttv.setText(Html.fromHtml(formattedText));
-                              // setline height to 1.5
-
-                              ttv.setLineSpacing(0,1.5f);
-
-                            }catch (Exception e){
-                                e.printStackTrace();
-
-                            }
-
-
-
-                            MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.wrong);
-                            mediaPlayer.start();
-                            invalidUser();
-
-
-
-
-                        }else{
-                            ttv.setText("User Details not Updated \n Please Contact Coordinator");
-
-                        }
-
-
-
+                        ttv.setLineSpacing(0, 1.5f);
 
                     } catch (Exception e) {
+
                         e.printStackTrace();
-                        invalidUser();
+                        Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show();
+
                     }
-                }, error -> invalidUser()
-        );
+
+
+                    MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.wrong);
+                    mediaPlayer.start();
+                    invalidUser();
+
+
+                } else {
+                    ttv.setText("User Details not Updated \n Please Contact Coordinator");
+
+                }
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                invalidUser();
+            }
+        }, error -> invalidUser());
         queue.add(stringRequest);
 
 
     }
+
     @SuppressLint("SetTextI18n")
-    void invalidUser(){
+    void invalidUser() {
         Button valid = findViewById(R.id.valid);
         valid.setVisibility(View.VISIBLE);
         View view = findViewById(R.id.view);
